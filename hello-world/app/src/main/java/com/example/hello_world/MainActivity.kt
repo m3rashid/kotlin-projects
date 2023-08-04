c7af200e27194ec5df1cc0ab3f9fdca89f6786ff
m3rashid <m3rashid.hussain@gmail.com>
2023-08-04 19:34:06 +0530

    I am BATMAN

--- hello-world/app/src/main/java/com/example/hello_world/MainActivity.kt
+++ hello-world/app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hello_world.ui.theme.HelloworldTheme
 
@@ -28,10 +29,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello World $name!",
             modifier = modifier
-    )
+        )
+    }
 }
 
 @Preview(showBackground = true)
